cab670fcd03633fadc0e5288063eaab5c1576253
Fidel <fidellauheinz@gmail.com>
2025-12-18 07:00:33 +0100

    First commit

---
+++ src/blackjack/useCases/pedir-carta.js
@@ -0,0 +1,15 @@
+// Esta función me permite tomar una carta
+
+/**
+ *
+ * @param {array<String>} //Pasamos el array de la baraja generada
+ * @returns {String} //Devolvemos el valor de la carta solicitada
+ */
+export const pedirCarta = (deck) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
---
+++ src/blackjack/useCases/valor-carta.js
@@ -0,0 +1,10 @@
+
+
+//Funcion para calcular la puntuación de la carta que le pedimos al deck
+export const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ?
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
