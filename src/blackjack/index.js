46599d4f3ca199cc18d1404fc554cf70f5472c5f
Fidel <fidellauheinz@gmail.com>
2026-01-07 12:06:03 +0100

    Tabulations saved

--- src/blackjack/index.js
+++ src/blackjack/index.js
@@ -9,18 +9,18 @@ import { crearCartaHtml, crearDeck, pedirCarta, turnoComputadora, valorCarta } f
  * 2S = Two of Spades
  */
 
-let deck         = [];
-const tipos      = ['C','D','H','S'];
-const especiales = ['A','J','Q','K'];
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
 
 let puntosJugador = 0;
 
 // Referencias del HTML
-const btnPedir   = document.querySelector('#btnPedir');
+const btnPedir = document.querySelector('#btnPedir');
 const btnDetener = document.querySelector('#btnDetener');
-const btnNuevo   = document.querySelector('#btnNuevo');
+const btnNuevo = document.querySelector('#btnNuevo');
 
-const divCartasJugador     = document.querySelector('#jugador-cartas');
+const divCartasJugador = document.querySelector('#jugador-cartas');
 const divCartasComputadora = document.querySelector('#computadora-cartas');
 
 const puntosHTML = document.querySelectorAll('small');
@@ -34,52 +34,52 @@ deck = crearDeck(tipos, especiales);
 btnPedir.addEventListener('click', () => {
 
     const carta = pedirCarta(deck);
-    
-    puntosJugador = puntosJugador + valorCarta( carta );
+
+    puntosJugador = puntosJugador + valorCarta(carta);
     puntosHTML[0].innerText = puntosJugador;
-    
-    const imgCarta = crearCartaHtml (carta)
-    divCartasJugador.append( imgCarta );
 
-    if ( puntosJugador > 21 ) {
+    const imgCarta = crearCartaHtml(carta)
+    divCartasJugador.append(imgCarta);
+
+    if (puntosJugador > 21) {
         console.warn('Lo siento mucho, perdiste');
-        btnPedir.disabled   = true;
+        btnPedir.disabled = true;
         btnDetener.disabled = true;
-        turnoComputadora( puntosJugador,puntosHTML[1],divCartasComputadora, deck);
+        turnoComputadora(puntosJugador, puntosHTML[1], divCartasComputadora, deck);
 
-    } else if ( puntosJugador === 21 ) {
+    } else if (puntosJugador === 21) {
         console.warn('21, genial!');
-        btnPedir.disabled   = true;
+        btnPedir.disabled = true;
         btnDetener.disabled = true;
-        turnoComputadora( puntosJugador,puntosHTML[1],divCartasComputadora, deck );
+        turnoComputadora(puntosJugador, puntosHTML[1], divCartasComputadora, deck);
     }
 
 });
 
 
 btnDetener.addEventListener('click', () => {
-    btnPedir.disabled   = true;
+    btnPedir.disabled = true;
     btnDetener.disabled = true;
 
-    turnoComputadora( puntosJugador,puntosHTML[1],divCartasComputadora, deck);
+    turnoComputadora(puntosJugador, puntosHTML[1], divCartasComputadora, deck);
 });
 
 btnNuevo.addEventListener('click', () => {
 
     console.clear();
     deck = [];
-    deck = crearDeck(tipos,especiales);
+    deck = crearDeck(tipos, especiales);
 
-    puntosJugador     = 0;
+    puntosJugador = 0;
 
-    
+
     puntosHTML[0].innerText = 0;
     puntosHTML[1].innerText = 0;
 
     divCartasComputadora.innerHTML = '';
     divCartasJugador.innerHTML = '';
 
-    btnPedir.disabled   = false;
+    btnPedir.disabled = false;
     btnDetener.disabled = false;
 
 });
--- src/blackjack/useCases/turno-computadora.js
+++ src/blackjack/useCases/turno-computadora.js
@@ -10,37 +10,37 @@ import { crearCartaHtml, pedirCarta, valorCarta } from "./";
  * @param {*} deck
  */
 
-export const turnoComputadora = ( puntosMinimos,puntosHTML,divCartasComputadora, deck= [] ) => {
+export const turnoComputadora = (puntosMinimos, puntosHTML, divCartasComputadora, deck = []) => {
 
-    
-    if (!puntosMinimos) throw new Error ('Puntos mínimos son necesarios');
-    if (!puntosHTML) throw new Error ('Puntos mínimos son necesarios');
+
+    if (!puntosMinimos) throw new Error('Puntos mínimos son necesarios');
+    if (!puntosHTML) throw new Error('Puntos mínimos son necesarios');
     let puntosComputadora = 0;
-    
+
     do {
         const carta = pedirCarta(deck);
 
-        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosComputadora = puntosComputadora + valorCarta(carta);
         puntosHTML.innerText = puntosComputadora;
-        
-        const imgCarta = crearCartaHtml(carta);
-        divCartasComputadora.append( imgCarta );
 
-        if( puntosMinimos > 21 ) {
+        const imgCarta = crearCartaHtml(carta);
+        divCartasComputadora.append(imgCarta);
+
+        if (puntosMinimos > 21) {
             break;
         }
 
-    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
 
     setTimeout(() => {
-        if( puntosComputadora === puntosMinimos ) {
+        if (puntosComputadora === puntosMinimos) {
             alert('Nadie gana :(');
-        } else if ( puntosMinimos > 21 ) {
+        } else if (puntosMinimos > 21) {
             alert('Computadora gana')
-        } else if( puntosComputadora > 21 ) {
+        } else if (puntosComputadora > 21) {
             alert('Jugador Gana');
         } else {
             alert('Computadora Gana')
         }
-    }, 100 );
+    }, 100);
 }
